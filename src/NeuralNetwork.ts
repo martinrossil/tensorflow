94afced98a0d31461cd7b71bfffdf9ae3ae4675e
Martin Rossil <martinrossil@gmail.com>
2023-06-21 12:34:10 +0200

    Created interface.

--- src/NeuralNetwork.ts
+++ src/NeuralNetwork.ts
@@ -1,11 +1,14 @@
+import ILevel from './ILevel';
+import INeuralNetwork from './INeuralNetwork';
 import Level from './Level';
 
-export default class NeuralNetwork extends HTMLElement {
-	public constructor() {
-		super();
-		console.log('NeuralNetwork()');
-		const level = new Level(4, 4);
-		console.log(level);
+export default class NeuralNetwork implements INeuralNetwork {
+	public levels: ILevel[];
+
+	public constructor(neuronCounts: number[]) {
+		this.levels = [];
+		for (let i = 0; i < neuronCounts.length - 1; i++) {
+			this.levels.push(new Level(neuronCounts[i], neuronCounts[i + 1]));
+		}
 	}
 }
-customElements.define('neural-network', NeuralNetwork);
